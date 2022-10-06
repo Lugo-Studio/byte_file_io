8c995589bf9adf4f1fff9fd90b828ebb87734094
Gabriel Lugo <g.alexander.lugo@gmail.com>
2022-10-06 04:46:46 -0400

    More reformatting

--- iobytes.hpp
+++ iobytes.hpp
@@ -10,72 +10,77 @@
 #include <ranges>
 #include <type_traits>
 
-namespace fx::io {
-  auto read_file_as_bytes(const std::filesystem::path& in_path) -> std::optional<std::vector<uint8_t>>
-  {
-    if (std::ifstream in{ in_path, std::ios::binary }; in.is_open()) {
-      const std::streamsize size{ static_cast<std::streamsize>(file_size(in_path)) };
-      std::vector<uint8_t> in_byte_buffer(size);
-      in.read(reinterpret_cast<char*>(in_byte_buffer.data()), size);
+namespace fx {
+  namespace io {
+    auto read_bytes(const std::filesystem::path& in_path) -> std::optional<std::vector<uint8_t>>
+    {
+      if (std::ifstream in{ in_path, std::ios::binary }; in.is_open()) {
+        const std::streamsize size{ static_cast<std::streamsize>(file_size(in_path)) };
+        std::vector<uint8_t> in_byte_buffer(size);
+        in.read(reinterpret_cast<char*>(in_byte_buffer.data()), size);
+        
+        return in_byte_buffer;
+      }
       
-      return in_byte_buffer;
+      return std::nullopt;
     }
     
-    return std::nullopt;
-  }
-  
-  auto write_bytes_to_file(const std::filesystem::path& out_path, const std::vector<uint8_t>& bytes)
-  {
-    if (std::ofstream out{ out_path, std::ios::binary }; out.is_open()) {
-      out.write(reinterpret_cast<const char*>(bytes.data()), static_cast<std::streamsize>(bytes.size()));
+    auto write_bytes(const std::filesystem::path& out_path, const std::vector<uint8_t>& bytes)
+    {
+      if (std::ofstream out{ out_path, std::ios::binary }; out.is_open()) {
+        out.write(reinterpret_cast<const char*>(bytes.data()), static_cast<std::streamsize>(bytes.size()));
+      }
     }
   }
   
-  template<std::ranges::range R>
-  auto write_to_file(R&& r, const std::filesystem::path& out_path)
+  namespace ranges {
+    template<std::ranges::range R>
+    auto write_to_file(R&& r, const std::filesystem::path& out_path)
     -> std::enable_if_t<std::is_same_v<std::ranges::range_value_t<R>, uint8_t>>
-  {
-    auto r_common = r | std::views::common;
-    std::vector<uint8_t> v;
-    
-    // if we can get a size, reserve that much
-    if constexpr (requires { std::ranges::size(r); }) {
-      v.reserve(std::ranges::size(r));
-    }
-    
-    v.insert(v.begin(), r_common.begin(), r_common.end());
-    
-    write_bytes_to_file(out_path, v);
-  }
-  
-  template <class Path>
-  concept path = requires(Path& p) {
-    std::filesystem::path();
-  };
-  
-  struct to_file_fn {
-    template<std::ranges::range Rng, path Path>
-    requires std::constructible_from<std::decay_t<Path>, Path>
-    constexpr void operator()(Rng&& r, Path out_path) const
-    noexcept(std::is_nothrow_constructible_v<std::decay_t<Path>, Path>)
     {
       auto r_common = r | std::views::common;
       std::vector<uint8_t> v;
+    
+      // if we can get a size, reserve that much
       if constexpr (requires { std::ranges::size(r); }) {
         v.reserve(std::ranges::size(r));
       }
-      v.insert(v.begin(), r_common.begin(), r_common.end());
     
-      write_bytes_to_file(out_path, v);
-    }
-
-    template <path Path>
-    requires std::constructible_from<std::decay_t<Path>, Path>
-    [[nodiscard]] constexpr auto operator()(Path out_path) const
-    noexcept(std::is_nothrow_constructible_v<std::decay_t<Path>, Path>) {
-      return std::ranges::_Range_closure<to_file_fn, decltype(out_path)>{ out_path };
-    }
-  };
+      v.insert(v.begin(), r_common.begin(), r_common.end());
   
-  inline constexpr to_file_fn write_byte_range;
+      fx::io::write_bytes(out_path, v);
+    }
+  
+    template<class Path>
+    concept path = requires(Path& p) {
+      std::filesystem::path();
+    };
+  
+    struct to_file_fn {
+      template<std::ranges::range Rng, path Path>
+      requires std::constructible_from<std::decay_t<Path>, Path>
+      constexpr void operator()(Rng&& r, Path out_path) const
+      noexcept(std::is_nothrow_constructible_v<std::decay_t<Path>, Path>)
+      {
+        auto r_common = r | std::views::common;
+        std::vector<uint8_t> v;
+        if constexpr (requires { std::ranges::size(r); }) {
+          v.reserve(std::ranges::size(r));
+        }
+        v.insert(v.begin(), r_common.begin(), r_common.end());
+      
+        fx::io::write_bytes(out_path, v);
+      }
+    
+      template<path Path>
+      requires std::constructible_from<std::decay_t<Path>, Path>
+      [[nodiscard]] constexpr auto operator()(Path out_path) const
+      noexcept(std::is_nothrow_constructible_v<std::decay_t<Path>, Path>)
+      {
+        return std::ranges::_Range_closure<to_file_fn, decltype(out_path)>{ out_path };
+      }
+    };
+    
+    inline constexpr to_file_fn write_bytes;
+  }
 } // fx
--- main.cpp
+++ main.cpp
@@ -11,13 +11,14 @@ int main()
   bool encrypted{ true };
   std::filesystem::path in_path{ "out.txt" };
   std::filesystem::path out_path{ "out2.txt" };
-  fx::if_optional(fx::io::read_file_as_bytes(in_path), [&](auto& in_bytes) {
+  fx::if_optional(fx::io::read_bytes(in_path), [&](auto& in_bytes) {
     std::default_random_engine eng{ 69 };
     std::uniform_int_distribution<int32_t> dist{ 0, 127 };
-    
-    std::views::transform(in_bytes, [&](uint8_t byte) {
-      uint8_t rand{ static_cast<uint8_t>(dist(eng)) };
-      return (byte + ((encrypted) ? 128 - rand : rand)) % 128;
-    }) | fx::io::write_byte_range(out_path);
+  
+    in_bytes | std::views::transform([&](uint8_t byte) {
+                 uint8_t rand{ static_cast<uint8_t>(dist(eng)) };
+                 return (byte + ((encrypted) ? 128 - rand : rand)) % 128;
+               })
+             | fx::ranges::write_bytes(out_path);
   });
 }
