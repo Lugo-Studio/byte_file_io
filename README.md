921ab122870aeecb92d45a71a95f3aedcf0ad3f5
Gabriel Lugo <g.alexander.lugo@gmail.com>
2022-10-06 05:12:36 -0400

    cli args

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # byte_file_io
 
 Just a simple set of headers that help for reading/writing bytes from files.
+
+Program usage: ./file_read [encrypt|decrypt] <input_file> <output_file> <encryption_seed_int32>
--- main.cpp
+++ main.cpp
@@ -2,22 +2,29 @@
 #include <random>
 #include <optional>
 #include <ranges>
+#include <cstdlib>
+#include <iostream>
 
 #include "iobytes.hpp"
 #include "if_optional.hpp"
 
-int main()
+int main(const int argc, char** argv)
 {
-  bool encrypted{ true };
-  std::filesystem::path in_path{ "out.txt" };
-  std::filesystem::path out_path{ "out2.txt" };
+  if (argc != 5) {
+    std::cerr << "Usage: [encrypt|decrypt] <input_file> <output_file> <encryption_seed_int32>\n";
+    return EXIT_FAILURE;
+  }
+  std::filesystem::path in_path{ argv[2] };
+  std::filesystem::path out_path{ argv[3] };
+  
   fx::if_optional(fx::io::read_bytes(in_path), [&](auto& in_bytes) {
-    std::default_random_engine eng{ 69 };
+    std::default_random_engine eng;
+    eng.seed(std::strtol(argv[4], nullptr, 10));
     std::uniform_int_distribution<int32_t> dist{ 0, 127 };
   
     in_bytes | std::views::transform([&](uint8_t byte) {
                  uint8_t rand{ static_cast<uint8_t>(dist(eng)) };
-                 return (byte + ((encrypted) ? 128 - rand : rand)) % 128;
+                 return (byte + ((std::strcmp(argv[1], "encrypt")) ? rand : 128 - rand)) % 128;
                })
              | fx::ranges::write_bytes(out_path);
   });
